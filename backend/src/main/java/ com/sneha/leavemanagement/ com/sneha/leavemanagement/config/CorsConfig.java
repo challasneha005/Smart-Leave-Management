88d79d09a870bb4d6a687944403310563cdb8930
Challa Sneha <challasneha005@gmail.com>
2026-09-17 20:17:34 +0530

    Delete backend/src/main/java/ com/sneha directory

--- backend/src/main/java/ com/sneha/leavemanagement/ com/sneha/leavemanagement/config/CorsConfig.java
+++
@@ -1,3 +0,0 @@
-package com.sneha.leavemanagement.config;
-import org.springframework.context.annotation.*; import org.springframework.web.servlet.config.annotation.*;
-@Configuration public class CorsConfig implements WebMvcConfigurer{ @Override public void addCorsMappings(CorsRegistry r){r.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS").allowedHeaders("*").allowCredentials(false);} }
