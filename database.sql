c3d1328fedf671cbb1bc909b6b25ac514fd1e8e1
Challa Sneha <challasneha005@gmail.com>
2026-09-17 21:04:26 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS employee_leave_management;
+USE employee_leave_management;
+-- The application uses Hibernate ddl-auto=update to create/update the schema.
+-- Run the Spring Boot application after creating this database.
