7ab3f3a50235545a4569ef13c128ac651cc89429
Challa Sneha <challasneha005@gmail.com>
2026-09-17 20:42:24 +0530

    Create CustomUserDetailsService.java

---
+++ backend/src/main/java/com/sneha/leavemanagement/security/CustomUserDetailsService.java
@@ -0,0 +1,10 @@
+package com.sneha.leavemanagement.security;
+import com.sneha.leavemanagement.model.User; 
+import com.sneha.leavemanagement.repository.UserRepository; 
+import org.springframework.security.core.userdetails.*; 
+import org.springframework.stereotype.Service;
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+ private final UserRepository repo; public CustomUserDetailsService(UserRepository repo){this.repo=repo;}
+ @Override public UserDetails loadUserByUsername(String username)throws UsernameNotFoundException{User u=repo.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("User not found")); return org.springframework.security.core.userdetails.User.withUsername(u.getUsername()).password(u.getPassword()).roles(u.getRole().name()).disabled(!u.isEnabled()).build();}
+}
